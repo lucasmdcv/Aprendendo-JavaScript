5a613a8c6d485497a13aabfbc72a8eb0b5e35a01
Lucas Mendes <111598752+lucasmdcv@users.noreply.github.com>
2022-08-31 11:39:57 -0300

    Create if2.js

---
+++ Controle.md/if2.js
@@ -0,0 +1,16 @@
+function teste1 (num){
+    if(num >= 7 )
+    console.log(num)
+    console.log('final')
+}
+
+teste1(5)
+teste1(9)
+
+function teste2 (num){
+if(num >7){     //nao usar ; com as estruturas de controle
+    console.log(num)
+}
+}
+teste2(6)
+teste2(8)
