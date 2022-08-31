af22ea1050c8d42a38beb6739913c972107de284
Lucas Mendes <111598752+lucasmdcv@users.noreply.github.com>
2022-08-31 11:40:16 -0300

    Create ifElse.js

---
+++ Controle.md/ifElse.js
@@ -0,0 +1,11 @@
+const imprimirResultado = function(nota){
+   if(nota >= 7)
+    console.log('aprovado!')
+   
+   else 
+      console.log('reprovado!')
+   
+}
+imprimirResultado(10)
+imprimirResultado(4)
+imprimirResultado('Epa!')
